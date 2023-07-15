ce6c9db5599ffcc3da588349b817fc3e746deecc
qkrmekem <magicofclown@naver.com>
2023-07-16 02:10:09 +0900

    before reupload

---
+++ src/backJoon/Test.java
@@ -0,0 +1,14 @@
+package backJoon;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(15%3);
+        System.out.println((int)Math.sqrt(30));
+        // auto commit test
+        // testtesetestsettst
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
