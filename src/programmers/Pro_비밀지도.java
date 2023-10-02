a00454eefc92458f4c035d9573bd8f512ffb51c3
qkrmekem <magicofclown@naver.com>
2023-10-02 23:12:21 +0900

    비밀지도 - 프로그래머스

--- src/programmers/Pro_비밀지도.java
+++ src/programmers/Pro_비밀지도.java
@@ -1,41 +1,66 @@
 package programmers;
 
 public class Pro_비밀지도 {
-    class Solution {
-        public String[] solution(int n, int[] arr1, int[] arr2) {
-            String[] answer = new String[n];
-            String tmp;
-            for(int i = 0; i < n; i++){
-                tmp = "";
-                int a = arr1[i];
-                String tmp1 = "";
-                while(a > 0){
-                    tmp1 = a%2 + tmp1;
-                    a = a/2;
-                }
-                int b = arr2[i];
-                String tmp2 = "";
-                while(b > 0){
-                    tmp2 = b%2 + tmp2;
-                    b = b/2;
-                }
-                if(n > tmp1.length()){
-                    tmp1 = "0".repeat(n-tmp1.length()) + tmp1;
-                }
-                if(n > tmp2.length()){
-                    tmp2 = "0".repeat(n-tmp2.length()) + tmp2;
-                }
-                for(int j = 0; j < n; j++){
-                    if(tmp1.charAt(j) == '1' || tmp2.charAt(j) == '1'){
-                        tmp = tmp + "#";
-                    }else{
-                        tmp = tmp + " ";
-                    }
-                }
-                answer[i] = tmp;
-
+    // 내 풀이
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        String tmp;
+        for(int i = 0; i < n; i++){
+            tmp = "";
+            int a = arr1[i];
+            String tmp1 = "";
+            while(a > 0){
+                tmp1 = a%2 + tmp1;
+                a = a/2;
             }
-            return answer;
+            int b = arr2[i];
+            String tmp2 = "";
+            while(b > 0){
+                tmp2 = b%2 + tmp2;
+                b = b/2;
+            }
+            if(n > tmp1.length()){
+                tmp1 = "0".repeat(n-tmp1.length()) + tmp1;
+            }
+            if(n > tmp2.length()){
+                tmp2 = "0".repeat(n-tmp2.length()) + tmp2;
+            }
+            for(int j = 0; j < n; j++){
+                if(tmp1.charAt(j) == '1' || tmp2.charAt(j) == '1'){
+                    tmp = tmp + "#";
+                }else{
+                    tmp = tmp + " ";
+                }
+            }
+            answer[i] = tmp;
+
         }
+        return answer;
+    }
+
+    // toBinaryString() 사용
+    public String[] solution2(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        String tmp;
+        for(int i = 0; i < n; i++){
+            tmp = "";
+            String num1 = Integer.toBinaryString(arr1[i]);
+            String num2 = Integer.toBinaryString(arr2[i]);
+            if(n > num1.length()){
+                num1 = "0".repeat(n-num1.length()) + num1;
+            }
+            if(n > num2.length()){
+                num2 = "0".repeat(n-num2.length()) + num2;
+            }
+            for(int j = 0; j < n; j++){
+                if(num1.charAt(j) == '1' || num2.charAt(j) == '1'){
+                    tmp += "#";
+                }else{
+                    tmp += " ";
+                }
+            }
+            answer[i] = tmp;
+        }
+        return answer;
     }
 }
