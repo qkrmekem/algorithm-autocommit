b57769057567146add1c7572a88295492b8e0307
qkrmekem <magicofclown@naver.com>
2023-10-22 22:54:23 +0900

    귤 고르기 - 프로그래머스

---
+++ src/programmers/Pro_귤고르기.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.*;
+
+public class Pro_귤고르기 {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        int[] arr = new int[10000001];
+        for(int i = 0; i < tangerine.length; i++){
+            arr[tangerine[i]]++;
+        }
+        Arrays.sort(arr);
+        int sum = 0;
+        for(int i = arr.length-1; i >= 0; i--){
+            answer++;
+            sum+=arr[i];
+            if(sum >= k){
+                break;
+            }
+        }
+        return answer;
+    }
+}
